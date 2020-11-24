c7fabdbafb6688991959fa55085f2b9025a96d3d
chendongdong <dongdong.chen@vonechain.com>
2020-11-24 23:22:11 +0800

    ios bug fix

--- src/ios/Canvas2ImagePlugin.m
+++ src/ios/Canvas2ImagePlugin.m
@@ -4,12 +4,13 @@
 //
 //  Created by Tommy-Carlos Williams on 29/03/12.
 //  Copyright (c) 2012 Tommy-Carlos Williams. All rights reserved.
-//	MIT Licensed
+//  MIT Licensed
 //
 
 #import "Canvas2ImagePlugin.h"
 #import <Cordova/CDV.h>
 #import <AssetsLibrary/AssetsLibrary.h>
+#import <Photos/Photos.h>
 
 @implementation Canvas2ImagePlugin
 @synthesize callbackId;
@@ -20,15 +21,19 @@
 //    return self;
 //}
 
-- (void)saveImageDataToLibrary:(CDVInvokedUrlCommand*)command
-{
+/*
+ 保存图片
+ */
+-(void) savaImg:(CDVInvokedUrlCommand*)command{
     self.callbackId = command.callbackId;
     NSData* imageData = [[NSData alloc] initWithBase64EncodedString:[command.arguments objectAtIndex:0] options:0];
 
     UIImage* image = [[[UIImage alloc] initWithData:imageData] autorelease];
-    ALAssetsLibrary *library = [[ALAssetsLibrary alloc] init];
-    // Request to save the image to camera roll
-    [library writeImageToSavedPhotosAlbum:[image CGImage] orientation:ALAssetOrientationUp /*(ALAssetOrientation)[image imageOrientation]*/ completionBlock:^(NSURL *assetURL, NSError *error){
+    //保存到相册
+    [[PHPhotoLibrary sharedPhotoLibrary] performChanges:^{
+        [PHAssetChangeRequest creationRequestForAssetFromImage:image];
+    } completionHandler:^(BOOL success, NSError * _Nullable error) {
+        //保存之后需要做的事情
         if (error) {
             // Show error message...
             NSLog(@"ERROR: %@",error);
@@ -37,16 +42,50 @@
         } else {
             // Show message image successfully saved
             NSLog(@"IMAGE SAVED!");
-            CDVPluginResult* result = [CDVPluginResult resultWithStatus: CDVCommandStatus_OK messageAsString: [assetURL absoluteString]];
+            CDVPluginResult* result = [CDVPluginResult resultWithStatus: CDVCommandStatus_OK];
             [self.commandDelegate sendPluginResult:result callbackId:self.callbackId];
         }
     }];
-    [library release];
+}
+- (void)saveImageDataToLibrary:(CDVInvokedUrlCommand*)command
+{
+    self.callbackId = command.callbackId;
+    PHAuthorizationStatus photoStatus = [PHPhotoLibrary authorizationStatus];
+    if (photoStatus==PHAuthorizationStatusAuthorized ) {
+        [self savaImg:command];
+    }else{
+        [PHPhotoLibrary requestAuthorization:^(PHAuthorizationStatus status) {
+            dispatch_async(dispatch_get_main_queue(),^{
+                NSString *errorMsg=nil;
+                switch (status) {
+                    case PHAuthorizationStatusAuthorized: //已获取权限
+                        [self savaImg:command];
+                        break;
+                    case PHAuthorizationStatusDenied:
+                    { //用户已经明确否认了这一照片数据的应用程序访问
+                        errorMsg=@"访问权限受限，请到设置里设置权限";
+                    }
+                        break;
+                    case PHAuthorizationStatusRestricted:
+                    {
+                        errorMsg=@"此应用程序没有被授权访问的照片数据。可能是家长控制权限";
+                    } break;
+                    default://其他。。。
+                        errorMsg=@"未知错误";
+                        break;
+                }
+                if(errorMsg!=nil){
+                    CDVPluginResult* result = [CDVPluginResult resultWithStatus: CDVCommandStatus_ERROR messageAsString:@"访问权限受限，请到设置里设置权限"];
+                    [self.commandDelegate sendPluginResult:result callbackId:self.callbackId];
+                }
+            });
+        }];
+    }
 }
 
 - (void)dealloc
-{	
-	[callbackId release];
+{   
+    [callbackId release];
     [super dealloc];
 }
 
